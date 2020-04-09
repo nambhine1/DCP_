a5a8fb85b04a944e6591314ab211f258e2dbcf38
rakotojaona <53557693+nambhine1@users.noreply.github.com>
2020-04-09 21:33:45 +0200

    Create dcp.cpp

---
+++ dcp.cpp
@@ -0,0 +1,49 @@
+#include "dcp_core.h"
+
+using namespace cv::ximgproc;
+
+void dehaze(IplImage *recover, IplImage *input)
+{
+	int height = input->height;
+	int width = input->width;
+	
+	IplImage *darkchannel = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
+	IplImage *transmission = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
+	IplImage *refine_transmission = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
+
+	int darkchannelradius = cvRound(MIN(width, height) * 0.02);
+	double Airlight[3] = { 0.0, 0.0, 0.0 };
+
+	TicToc t;
+	t.tic();
+	printf("CalcDarkChannel...");
+	CalcDarkChannel(darkchannel, input, darkchannelradius);
+	t.toc();
+
+	t.tic();
+	printf("CalcAirlight...");
+	CalcAirlight(darkchannel, input, Airlight);
+	t.toc();
+
+	t.tic();
+	printf("CalcTransmission...");
+	CalcTransmission(transmission, input, Airlight, darkchannelradius);
+	t.toc();
+
+	t.tic();
+	printf("GuidedFilterColor...");
+
+
+	GuidedFilterColor(refine_transmission, input, transmission, 1e-6, 60);
+	// guidedFilter(cv::cvarrToMat(input), cv::cvarrToMat(transmission), cv::cvarrToMat(refine_transmission), 60, 1e-6);
+	t.toc();
+
+	t.tic();
+	printf("CalcRecover...");
+	CalcRecover(recover, input, refine_transmission, Airlight);	
+	t.toc();
+
+	cvReleaseImage(&refine_transmission);
+	cvReleaseImage(&transmission);
+	cvReleaseImage(&darkchannel);
+}
